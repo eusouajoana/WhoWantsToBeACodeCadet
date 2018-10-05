6f373cbd40a64c6667c2451e7fc052746d145f63
Joao Mendes <joaomendes@Joaos-MBP.lan>
2018-10-06 00:23:33 +0100

    added simplegraphics classes and <AC_> tester

---
+++ whowanttobeacodecadet/src/org/academiadecodigo/variachis/fila2/whowanttobeacodecadet/simplegraphics/keyboard/KeyboardEventType.java
@@ -0,0 +1,10 @@
+package org.academiadecodigo.variachis.fila2.whowanttobeacodecadet.simplegraphics.keyboard;
+
+/**
+ * The type of events supported by the Keyboard
+ * @see Keyboard
+ */
+public enum KeyboardEventType {
+    KEY_PRESSED,
+    KEY_RELEASED
+}
---
+++ whowanttobeacodecadet/src/org/academiadecodigo/variachis/fila2/whowanttobeacodecadet/simplegraphics/mouse/MouseEventType.java
@@ -0,0 +1,10 @@
+package org.academiadecodigo.variachis.fila2.whowanttobeacodecadet.simplegraphics.mouse;
+
+/**
+ * The type of events supported by the Mouse
+ * @see Mouse
+ */
+public enum MouseEventType {
+    MOUSE_CLICKED,
+    MOUSE_MOVED
+}
