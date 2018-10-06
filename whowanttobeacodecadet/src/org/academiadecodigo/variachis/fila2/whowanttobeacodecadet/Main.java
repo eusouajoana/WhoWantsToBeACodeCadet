2148f13d913b20e5ac755a2e6ddbfef1f3b89c2c
Joao Mendes <joaomendes@Joaos-MBP.lan>
2018-10-06 16:51:35 +0100

    first picture working

--- whowanttobeacodecadet/src/org/academiadecodigo/variachis/fila2/whowanttobeacodecadet/Main.java
+++ whowanttobeacodecadet/src/org/academiadecodigo/variachis/fila2/whowanttobeacodecadet/Main.java
@@ -1,4 +1,5 @@
 package org.academiadecodigo.variachis.fila2.whowanttobeacodecadet;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 import org.academiadecodigo.variachis.fila2.whowanttobeacodecadet.trivialpursuit.Board;
 
 public class Main {
@@ -15,5 +16,9 @@ public class Main {
        /* Board board = new Board();
         board.position();
         board.paths("1e1",2); */
+
+       //Print <AC_> logo with SimpleGraphics
+        Picture pic = new Picture(20, 100, "resources/img/ac_logo.png");
+        pic.draw();
     }
 }
