54411c783ef19bb566a45bebc91be9bdd887ff81
Luis Jeronimo <drbluis@gmail.com>
2018-10-08 18:14:15 +0100

    added branch

---
+++ whowanttobeacodecadet/src/org/academiadecodigo/variachis/fila2/whowanttobeacodecadet/trivialpursuit/grid/QuestionGfx.java
@@ -0,0 +1,18 @@
+package org.academiadecodigo.variachis.fila2.whowanttobeacodecadet.trivialpursuit.grid;
+
+
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.variachis.fila2.whowanttobeacodecadet.trivialpursuit.grid.position.GridPosition;
+import org.academiadecodigo.variachis.fila2.whowanttobeacodecadet.trivialpursuit.grid.position.SimpleGfxGridPosition;
+
+
+public class QuestionGfx {
+
+
+
+  Rectangle rect = new Rectangle(10,10,400,400);
+
+
+
+
+}
