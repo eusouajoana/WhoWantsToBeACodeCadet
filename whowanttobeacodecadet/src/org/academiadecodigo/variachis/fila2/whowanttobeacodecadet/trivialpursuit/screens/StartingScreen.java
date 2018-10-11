42ea6289eaa37d88c13a2bab8ee737c92ffa072f
Kiteres <kiteres.code@gmail.com>
2018-10-11 20:43:17 +0100

    start screen

--- whowanttobeacodecadet/src/org/academiadecodigo/variachis/fila2/whowanttobeacodecadet/trivialpursuit/screens/StartingScreen.java
+++ whowanttobeacodecadet/src/org/academiadecodigo/variachis/fila2/whowanttobeacodecadet/trivialpursuit/screens/StartingScreen.java
@@ -6,6 +6,7 @@ import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 import org.academiadecodigo.variachis.fila2.whowanttobeacodecadet.Game;
+import org.academiadecodigo.variachis.fila2.whowanttobeacodecadet.Player;
 
 public class StartingScreen implements KeyboardHandler {
 
@@ -15,38 +16,44 @@ public class StartingScreen implements KeyboardHandler {
     private String path;
     private Game game;
     private Keyboard k;
-    private KeyboardEvent event;
+    private KeyboardEvent space;
+    private Player player;
 
-    public StartingScreen(int var1, int var2, String var3) {
 
-        this.x = var1;
-        this.y = var2;
-        this.path = var3;
-        this.startingScreen = new Picture((double)this.x, (double)this.y, var3);
+
+    public StartingScreen(int col, int row, String path) {
+
+        this.x = col;
+        this.y = row;
+        this.path = path;
+        this.startingScreen = new Picture((double)this.x, (double)this.y, path);
         this.startingScreen.draw();
+
+
     }
 
-    public void startKey() {
+    public void startKey() throws InterruptedException {
 
-        this.k = new Keyboard(this);
-        this.event = new KeyboardEvent();
-        this.event.setKey(32);
-        this.event.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-        this.k.addEventListener(this.event);
+        k = new Keyboard(this);
+        space = new KeyboardEvent();
+        space.setKey(KeyboardEvent.KEY_SPACE);
+        space.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        k.addEventListener(space);
     }
 
-    public void keyPressed(KeyboardEvent var1) {
+    @Override
+    public void keyPressed(KeyboardEvent event) {
 
-        if (var1.getKey() == 32) {
+        if (event.equals(space) ) {
 
-            this.startingScreen.delete();
-            this.game = new Game();
-            this.game.start();
-            this.k.removeEventListener(this.event);
+            startingScreen.delete();
+            game = new Game();
+            game.start();
+            k.removeEventListener(space);
         }
     }
 
-    public void keyReleased(KeyboardEvent var1) {
+    public void keyReleased(KeyboardEvent event) {
 
     }
 
--- whowanttobeacodecadet/src/org/academiadecodigo/variachis/fila2/whowanttobeacodecadet/trivialpursuit/screens/tester.java
+++ whowanttobeacodecadet/src/org/academiadecodigo/variachis/fila2/whowanttobeacodecadet/trivialpursuit/screens/tester.java
@@ -1,9 +1,18 @@
 package org.academiadecodigo.variachis.fila2.whowanttobeacodecadet.trivialpursuit.screens;
 
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
 public class tester {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        StartingScreen startingScreen = new StartingScreen(25, 25, "resources/img/ac_logo.png");
+
+
+        StartingScreen startingScreen = new StartingScreen(10, 10, "resources/img/startscreen.png");
+
+        startingScreen.startKey();
+
+
     }
 }
